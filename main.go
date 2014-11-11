613c959c1d917c84421b35926ee6bfd4d7d01051
Ryan Schmukler <rschmukler@gmail.com>
2014-11-10 23:38:27 -0500

    fix example

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-  "log"
-  "github.com/coreos/go-etcd/etcd"
+	"github.com/coreos/go-etcd/etcd"
+	"log"
 )
 
 func main() {
-  machines := []string{"192.168.59.3:49159"}
-  c := etcd.NewClient(machines)
-  _, err := c.Get("some-message", false, false)
-  if err != nil {
-    log.Fatal(err)
-  }
-  comChannel := make(chan *etcd.Response)
-  c.Watch("/some-message", 0, false, comChannel, nil)
-  for {
-    select {
-      case msg := <- comChannel:
-        log.Printf("%s", msg.Node.Key)
-        break
-    }
-  }
+	c := etcd.NewClient([]string{"http://192.168.59.3:49159"})
+	comChannel := make(chan *etcd.Response)
+	_, err := c.Watch("/some-message", 0, false, comChannel, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		select {
+		case msg := <-comChannel:
+			log.Printf("%s", msg.Node.Key)
+		}
+	}
 }
